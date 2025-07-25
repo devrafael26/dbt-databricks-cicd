533877f824c888c3b0dfdd0703340799fec45e0d
devrafael26 <105223642+devrafael26@users.noreply.github.com>
2025-07-25 13:08:53 -0300

    retirada de model avg_ticket_teste

--- dbt_databricks_cicd/models/mart/avg_ticket_by_category.sql
+++
@@ -1,9 +0,0 @@
-
-{{ config(materialized='view') }}
-
-SELECT
-    Category,
-    ROUND(AVG(CAST(REPLACE(`Final_Price`, ',', '.') AS DOUBLE)), 2) AS avg_ticket
-FROM {{ ref('stg_ecommerce') }}
-GROUP BY Category
-ORDER BY avg_ticket DESC
