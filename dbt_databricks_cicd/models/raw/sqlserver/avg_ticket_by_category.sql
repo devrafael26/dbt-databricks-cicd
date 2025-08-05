c36712cf5beb589e6300c9b3da2d3d2fc32f2e20
devrafael26 <105223642+devrafael26@users.noreply.github.com>
2025-08-05 14:26:59 -0300

    Atualiza estrutura de pastas, models e schemas por target (sqlserver e databricks)

--- dbt_databricks_cicd/models/raw/sqlserver/avg_ticket_by_category.sql
+++
@@ -1,9 +0,0 @@
-{{ config(materialized='view') }}
-
-SELECT
-  Category,
-  ROUND(AVG(CAST(REPLACE(Final_Price, ',', '.') AS FLOAT)), 2) AS avg_ticket
-FROM {{ ref('stg_ecommerce') }}
-GROUP BY Category
-ORDER BY avg_ticket DESC
-
