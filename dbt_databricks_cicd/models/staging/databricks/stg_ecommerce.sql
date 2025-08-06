44a1d7ae5563d2f64098fb5fc844335a2b143e88
devrafael26 <105223642+devrafael26@users.noreply.github.com>
2025-08-06 10:13:39 -0300

    Correção de estrutura de pastas

--- dbt_databricks_cicd/models/staging/databricks/stg_ecommerce.sql
+++ dbt_databricks_cicd/models/staging/databricks/stg_ecommerce.sql
@@ -1,16 +1,15 @@
 {{ config(
-    enabled=(target.name == 'sqlserver'),
+    enabled=(target.name == 'databricks'),
     materialized='view'
 ) }}
 
 SELECT
-  CAST(User_ID AS STRING) AS User_ID,
-  CAST(Product_ID AS STRING) AS Product_ID,
-  CAST(Category AS STRING) AS Category,
-  CAST(Price AS DOUBLE) AS Price,
-  CAST(Discount AS INT) AS Discount,
-  CAST(Final_Price AS DOUBLE) AS Final_Price,
-  CAST(Payment_Method AS STRING) AS Payment_Method,
-  CAST(Purchase_Date AS DATE) AS Purchase_Date
-FROM {{ source('sqlserver_data', 'ecommerce') }}
-
+  User_ID,
+  Product_ID,
+  Category,
+  Price,
+  Discount,
+  Final_Price,
+  Payment_Method,
+  Purchase_Date
+FROM {{ ref('src_ecommerce') }}
---
+++ dbt_databricks_cicd/models/staging/sqlserver/stg_ecommerce.sql
@@ -0,0 +1,16 @@
+{{ config(
+    enabled=(target.name == 'sqlserver'),
+    materialized='view'
+) }}
+
+SELECT
+  CAST(User_ID AS STRING) AS User_ID,
+  CAST(Product_ID AS STRING) AS Product_ID,
+  CAST(Category AS STRING) AS Category,
+  CAST(Price AS DOUBLE) AS Price,
+  CAST(Discount AS INT) AS Discount,
+  CAST(Final_Price AS DOUBLE) AS Final_Price,
+  CAST(Payment_Method AS STRING) AS Payment_Method,
+  CAST(Purchase_Date AS DATE) AS Purchase_Date
+FROM {{ source('sqlserver_data', 'ecommerce') }}
+
